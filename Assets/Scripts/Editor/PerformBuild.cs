b0630f20e580cded5866057866f8ce1d801cbb0c
martinparachu <martin.parachu@globant.com>
2020-04-03 17:22:54 -0300

    initial load

---
+++ Assets/Scripts/Editor/PerformBuild.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+class BuildSrcipt
+{
+    static string[] scenes = { "Assets/Scenes/CSharpScene.unity" };
+    static string name = "HelloWorld";
+
+
+    static void BuildLinux()
+    {
+        BuildPipeline.BuildPlayer(scenes, "./" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64, BuildOptions.None);
+    }
+
+    static void BuildMac()
+    {
+        BuildPipeline.BuildPlayer(scenes, "./" + name + "_Mac/" + name, BuildTarget.StandaloneOSX, BuildOptions.None);
+    }
+}
+
